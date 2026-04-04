53f40932267b4e7665ede0a415aed06ec5e83053
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-04 10:03:37 -0300

    build: create project using spring boot

---
+++ src/main/java/dev/vinicius/elifoot/ElifootApplication.java
@@ -0,0 +1,13 @@
+package dev.vinicius.elifoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElifootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElifootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vinicius/elifoot/ElifootApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vinicius.elifoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElifootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
