37f417e2e02cbb370817a181abbd3f5dde8a6186
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-11 09:35:04 -0300

    feat: create find stadium service

--- src/main/java/dev/vinicius/elifoot/service/FindStadiumService.java
+++ src/main/java/dev/vinicius/elifoot/service/FindStadiumService.java
@@ -1,4 +1,20 @@
 package dev.vinicius.elifoot.service;
 
+import dev.vinicius.elifoot.entity.Stadium;
+import dev.vinicius.elifoot.repository.StadiumRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FindStadiumService {
+    private final StadiumRepository stadiumRepository;
+
+    public FindStadiumService(StadiumRepository stadiumRepository) {
+        this.stadiumRepository = stadiumRepository;
+    }
+
+    public Page<Stadium> findAll(Pageable pageable){
+        return this.stadiumRepository.findAll(pageable);
+    }
 }
