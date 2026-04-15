66dc660a4e1974e196a123a7079819cc3efe22cc
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-15 13:45:21 -0300

    refactor: implement mapStruct on stadium mapper

--- src/main/java/dev/vinicius/elifoot/mapper/StadiumMapper.java
+++ src/main/java/dev/vinicius/elifoot/mapper/StadiumMapper.java
@@ -3,26 +3,11 @@ package dev.vinicius.elifoot.mapper;
 import dev.vinicius.elifoot.controller.request.StadiumRequest;
 import dev.vinicius.elifoot.controller.response.StadiumResponse;
 import dev.vinicius.elifoot.entity.Stadium;
+import org.mapstruct.Mapper;
 
-public class StadiumMapper {
+@Mapper(componentModel = "spring")
+public interface StadiumMapper {
 
-    public static StadiumResponse toResponse(Stadium stadium){
-        return StadiumResponse.builder()
-                .id(stadium.getId())
-                .name(stadium.getName())
-                .city(stadium.getCity())
-                .capacity(stadium.getCapacity())
-                .urlImg(stadium.getUrlImg())
-                .build();
-    }
-
-    public static Stadium toDomain(StadiumRequest stadiumRequest){
-        return Stadium.builder()
-                .name(stadiumRequest.getName())
-                .city(stadiumRequest.getCity())
-                .capacity(stadiumRequest.getCapacity())
-                .urlImg(stadiumRequest.getUrlImage())
-                .build();
-               
-    }
+    StadiumResponse toResponse(Stadium stadium);
+    Stadium toDomain(StadiumRequest stadiumRequest);
 }
