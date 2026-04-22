ed3c66b6c086e67dc8f46d943625c7d14b921c84
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-22 09:39:53 -0300

    feat: implement route to find all clubs

--- src/main/java/dev/vinicius/elifoot/controller/ClubController.java
+++ src/main/java/dev/vinicius/elifoot/controller/ClubController.java
@@ -1,4 +1,39 @@
 package dev.vinicius.elifoot.controller;
 
+import dev.vinicius.elifoot.controller.response.ClubResponse;
+import dev.vinicius.elifoot.service.FindClubService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/clubs")
 public class ClubController {
+
+    private final FindClubService findClubService
+
+    public ClubController(FindClubService findClubService) {
+        this.findClubService = findClubService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClubResponse>> findAll(Pageable pageable){
+        Page<ClubResponse> clubResponses = this.findClubService.findAll(pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(clubResponses);
+    }
+
+    @GetMapping("/{id}")
+    public void finById(){
+    }
+
+    @PostMapping
+    public void create(){
+
+    }
 }
