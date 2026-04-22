c475f8ec5180b50b38aa5a53b07741f68be94486
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-22 10:01:07 -0300

    feat: create service to find club by id

--- src/main/java/dev/vinicius/elifoot/mapper/ClubMapper.java
+++ src/main/java/dev/vinicius/elifoot/mapper/ClubMapper.java
@@ -1,5 +1,6 @@
 package dev.vinicius.elifoot.mapper;
 
+import dev.vinicius.elifoot.controller.response.ClubDetailsResponse;
 import dev.vinicius.elifoot.controller.response.ClubResponse;
 import dev.vinicius.elifoot.entity.Club;
 import org.mapstruct.Mapper;
@@ -7,4 +8,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface ClubMapper {
     ClubResponse toResponse(Club club);
+    ClubDetailsResponse toDetailsResponse(Club club);
 }
--- src/main/java/dev/vinicius/elifoot/service/FindClubService.java
+++ src/main/java/dev/vinicius/elifoot/service/FindClubService.java
@@ -1,11 +1,16 @@
 package dev.vinicius.elifoot.service;
 
+import dev.vinicius.elifoot.controller.response.ClubDetailsResponse;
 import dev.vinicius.elifoot.controller.response.ClubResponse;
+import dev.vinicius.elifoot.entity.Club;
 import dev.vinicius.elifoot.mapper.ClubMapper;
 import dev.vinicius.elifoot.repository.ClubRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class FindClubService {
@@ -19,5 +24,11 @@ public class FindClubService {
 
     public Page<ClubResponse> findAll(Pageable pageable){
         return this.clubRepository.findAll(pageable).map(this.clubMapper::toResponse);
+    }
+
+    public Optional<ClubDetailsResponse> findById(String clubId){
+        return this.clubRepository
+                .findById(UUID.fromString(clubId))
+                .map(this.clubMapper::toDetailsResponse);
     }
 }
