2ed2d920e748f98c9dd9ddcaa57a5361acafb0d3
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-25 09:06:01 -0300

    feat(swagger): add OpenAPI schema annotations to player DTOs

--- src/main/java/dev/vinicius/elifoot/controller/request/PlayerRequest.java
+++ src/main/java/dev/vinicius/elifoot/controller/request/PlayerRequest.java
@@ -1,5 +1,7 @@
 package dev.vinicius.elifoot.controller.request;
+
 import dev.vinicius.elifoot.entity.Position;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -9,15 +11,25 @@ import lombok.Setter;
 @Builder
 @Getter
 @Setter
+@Schema(description = "Payload to register a new player")
 public class PlayerRequest {
 
     @NotBlank
+    @Schema(description = "Player full name", example = "Gabigol")
     private String name;
+
     @NotNull
+    @Schema(description = "Player position on the field", example = "FORWARD")
     private Position position;
+
     @NotNull
+    @Schema(description = "Shirt number", example = "10")
     private int shirtNumber;
+
+    @Schema(description = "URL of the player photo")
     private String urlImg;
+
     @NotNull
+    @Schema(description = "ID of the club the player belongs to", example = "1")
     private Long clubId;
 }
--- src/main/java/dev/vinicius/elifoot/controller/response/PlayerResponse.java
+++ src/main/java/dev/vinicius/elifoot/controller/response/PlayerResponse.java
@@ -1,6 +1,7 @@
 package dev.vinicius.elifoot.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,12 +13,22 @@ import java.util.UUID;
 @AllArgsConstructor
 @Getter
 @Setter
+@Schema(description = "Summarized player data")
 public class PlayerResponse {
 
+    @Schema(description = "Player UUID")
     private UUID id;
+
+    @Schema(description = "Player full name", example = "Gabigol")
     private String name;
+
+    @Schema(description = "Player position label", example = "Forward")
     private String position;
+
+    @Schema(description = "Shirt number", example = "10")
     private int shirtNumber;
+
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Schema(description = "URL of the player photo")
     private String urlImg;
 }
