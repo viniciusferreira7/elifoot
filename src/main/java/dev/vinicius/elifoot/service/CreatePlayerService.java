0382fee741915ed2cc4b4381838b3eb49857bb87
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-25 08:48:50 -0300

    feat(services): create service to create player

--- src/main/java/dev/vinicius/elifoot/service/CreatePlayerService.java
+++ src/main/java/dev/vinicius/elifoot/service/CreatePlayerService.java
@@ -1,4 +1,22 @@
 package dev.vinicius.elifoot.service;
 
+import dev.vinicius.elifoot.controller.request.PlayerRequest;
+import dev.vinicius.elifoot.controller.response.PlayerResponse;
+import dev.vinicius.elifoot.entity.Player;
+import dev.vinicius.elifoot.mapper.PlayerMapper;
+import dev.vinicius.elifoot.repository.PlayerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CreatePlayerService {
+    private final PlayerRepository playerRepository;
+    private final PlayerMapper playerMapper;
+
+    public PlayerResponse create(PlayerRequest playerRequest){
+        Player player = this.playerRepository.save(this.playerMapper.toEntity(playerRequest));
+
+        return this.playerMapper.toResponse(player);
+    }
 }
