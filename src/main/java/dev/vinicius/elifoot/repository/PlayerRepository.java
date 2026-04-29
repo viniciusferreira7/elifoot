905182b6a7f320b07873dc28865c02805e3bf908
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-04-29 10:32:29 -0300

    feat(repositories): add method to find player by club id

--- src/main/java/dev/vinicius/elifoot/repository/PlayerRepository.java
+++ src/main/java/dev/vinicius/elifoot/repository/PlayerRepository.java
@@ -1,9 +1,12 @@
 package dev.vinicius.elifoot.repository;
 
 import dev.vinicius.elifoot.entity.Player;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface PlayerRepository extends JpaRepository<Player, UUID> {
+    Page<Player> findByClubId(UUID clubId, Pageable pageable);
 }
