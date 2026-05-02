d0b103c0731b17f293daacb8afe1fdf9cbbb9fe4
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-02 08:36:18 -0300

    refactor(repository): add annotation to avoid multiples queries

--- src/main/java/dev/vinicius/elifoot/repository/PlayerRepository.java
+++ src/main/java/dev/vinicius/elifoot/repository/PlayerRepository.java
@@ -3,10 +3,13 @@ package dev.vinicius.elifoot.repository;
 import dev.vinicius.elifoot.entity.Player;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface PlayerRepository extends JpaRepository<Player, UUID> {
+
+    @EntityGraph(attributePaths = "club")
     Page<Player> findByClubId(UUID clubId, Pageable pageable);
 }
