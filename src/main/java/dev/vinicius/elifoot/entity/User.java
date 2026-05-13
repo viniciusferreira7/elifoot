101eb5dffda139b475229d4904a4bc2156dfe14d
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-13 12:48:43 -0300

    feat(user): add fetch type eager

--- src/main/java/dev/vinicius/elifoot/entity/User.java
+++ src/main/java/dev/vinicius/elifoot/entity/User.java
@@ -34,7 +34,7 @@ public class User {
     @Column(nullable = false )
     private Boolean active = true;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "users_scopes",
             joinColumns = @JoinColumn(name = "user_id"),
