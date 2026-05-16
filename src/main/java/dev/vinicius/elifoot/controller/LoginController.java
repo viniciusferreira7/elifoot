b045dc020721b50de067535642d10c3881b83adc
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-16 12:26:45 -0300

    feat(controller): add login service on login controller

--- src/main/java/dev/vinicius/elifoot/controller/LoginController.java
+++ src/main/java/dev/vinicius/elifoot/controller/LoginController.java
@@ -1,7 +1,10 @@
 package dev.vinicius.elifoot.controller;
 
 import dev.vinicius.elifoot.controller.request.LoginRequest;
+import dev.vinicius.elifoot.controller.response.LoginResponse;
+import dev.vinicius.elifoot.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users/login")
 @RequiredArgsConstructor
 public class LoginController {
-    private final FIndUserService fIndUserService;
+    private final LoginService loginService;
 
     @PostMapping
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
-        return fIndUserService.login(loginRequest);
+        LoginResponse loginResponse = this.loginService.login(loginRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
     }
 }
