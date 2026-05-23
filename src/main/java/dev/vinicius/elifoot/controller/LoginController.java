04fcc0ffd14c7e3880b61b4712ebb08e19f5ab25
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2026-05-23 07:58:02 -0300

    feat(swagger): add swagger annotations on login controller

--- src/main/java/dev/vinicius/elifoot/controller/LoginController.java
+++ src/main/java/dev/vinicius/elifoot/controller/LoginController.java
@@ -3,6 +3,12 @@ package dev.vinicius.elifoot.controller;
 import dev.vinicius.elifoot.controller.request.LoginRequest;
 import dev.vinicius.elifoot.controller.response.LoginResponse;
 import dev.vinicius.elifoot.service.LoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +20,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users/login")
 @RequiredArgsConstructor
+@Tag(name = "Authentication", description = "User authentication API")
 public class LoginController {
     private final LoginService loginService;
 
+    @Operation(summary = "User login", description = "Authenticates the user and returns an authentication token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login successful",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = LoginResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content),
+            @ApiResponse(responseCode = "401", description = "Invalid credentials", content = @Content)
+    })
     @PostMapping
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
         LoginResponse loginResponse = this.loginService.login(loginRequest);
